cf60331cfb6f84aac23dc44129608c430bdfffbb
smateusx <smateux10@gmail.com>
2025-07-24 16:21:49 -0300

    finalizei 2 ex

---
+++ ex001/desafio.js
@@ -0,0 +1,6 @@
+let raio = 10;
+const PI = 3.14;
+let res = PI * raio * raio;
+
+console.log(`A área da circunferencia é ${res}m²`)
+
---
+++ ex001/desafio2.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 94;
+
+let temp = a; // criou uma variavel temporaria 
+
+a = b;
+b = temp;
+
+
+
+console.log(a, b)
