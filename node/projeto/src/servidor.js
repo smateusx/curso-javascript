7939900f0bcb02141014fcf35435d1d81f61e236
smateusx <smateux10@gmail.com>
2025-08-19 10:04:38 -0300

    finalizei node

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,9 +2,25 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./bancoDeDados')
+
+app.use(bodyParser.urlencoded({extend: true}))
 
 app.get('/produtos', (req, res, next) => {
-    res.send({nome: 'Notebook', preco: 1259.90}) //converte para JSON
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)//JSON
 })
 
 app.listen(porta, () => {
---
+++ node/temporizador.js
@@ -0,0 +1,11 @@
+const schedule = require('node-schedule')
+
+const tarefa1 = schedule.scheduleJob('*/5 * 10 * * 2', function() {
+    console.log('Executando Tarefa 1!', new Date().getSeconds())
+})
+
+setTimeout(function() {
+    tarefa1.cancel()
+    console.log('Cancelando Tarefa 1!')
+},20000)
+
